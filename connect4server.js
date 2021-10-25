df1a9c6cec1e90c38b8829e338403031ff2f6312
anshuup <anshupatel@cmail.carleton.ca>
2021-10-24 23:24:49 -0400

    Update connect4server.js

--- connect4server.js
+++ connect4server.js
@@ -32,7 +32,7 @@ let userMode;
 
 
 app.get("/",(req,res) => {
-	res.sendFile(path.join(__dirname, "connect4.html"))
+	res.sendFile(path.join(__dirname, "/connect4.html"))
 })
 app.get("/playGuest",(req,res)=>{
 	res.sendFile(path.join(__dirname, "/playGuest.html"))
@@ -823,8 +823,9 @@ function displayfriendStat(req,res,next){
 	})
 	next()
 }
+const port = process.env.PORT || 3000
 
-http.listen(3000,() =>{
+http.listen(port,() =>{
 	console.log('Server running at http://127.0.0.1:3000/');
 });
 let color = "red"
